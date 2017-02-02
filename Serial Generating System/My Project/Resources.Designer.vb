ddd32fd0fc2d18597746ba48f36193de618b76f7
PhirompornBussaban <phirompornbussaban@gmail.com>
2017-02-02 07:58:20 +0700

    Welcome form New background

--- Serial Generating System/My Project/Resources.Designer.vb
+++ Serial Generating System/My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property bgBlurBlueNewNoLogo() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("bgBlurBlueNewNoLogo", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property bgBlurColor() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("bgBlurColor", resourceCulture)
