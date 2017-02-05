3ccac259afe530427eef6dbb16ada8d4f5604a3b
pratya yeekhaday <zee_pratya@hotmail.com>
2017-02-05 22:36:50 +0700

    add check duplicate realtime

--- Serial Generating System/Administrator/EditUser.vb
+++ Serial Generating System/Administrator/EditUser.vb
@@ -20,20 +20,19 @@ Public Class EditUser
         InitializeComponent()
         ' Add any initialization after the InitializeComponent() call.
         Me.id = id
-
     End Sub
     Private Sub Edit_User_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim username As String = Login.user
         Dim password As String = Login.pass
-        txtAccountInfo.Text = username.ToString
-        tvAdminMenu.ExpandAll()
+        lblMsgEmpId.Visible = False
+        pbEmpId.Visible = False
+        lblMsgEmail.Visible = False
+        pbEmail.Visible = False
         loadData()
         If sqlReader.Read Then
             initialData()
         End If
-
         convertData()
-
     End Sub
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnSave.Click
         If isFromValid() AndAlso Not checkDuplicate() Then
@@ -49,7 +48,6 @@ Public Class EditUser
     Private Sub chbPerEdit_CheckedChanged(sender As Object, e As EventArgs) Handles chbPerEdit.CheckedChanged
         perEdit = IIf(chbPerEdit.Checked, 1, 0)
     End Sub
-
     Private Function isFromValid() As Boolean
         Dim valid = True
         Dim regexCharAndNumber = "^[0-9a-zA-Z]*$"
@@ -77,13 +75,11 @@ Public Class EditUser
             MessageBox.Show("กรุณากรอกอีเมล")
             valid = False
         ElseIf Not New Regex(EmailRegex).IsMatch(txtEmail.Text.Trim) Then
-            MessageBox.Show("รูปแบบอีเมลไม่ถูกต้อง")
+            MessageBox.Show("รูปแบบอีเมลไม่ถูกต้อง ตัวอย่าง example@example.example")
             valid = False
-
         End If
         Return valid
     End Function
-
     Private Sub cmbUserType_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbUserType.SelectedIndexChanged
         userType = IIf("ผู้ดูแลระบบ".Equals(cmbUserType.SelectedItem), "admin", "user")
     End Sub
@@ -91,23 +87,12 @@ Public Class EditUser
         Me.Hide()
         SearchUser.Show()
     End Sub
-
     Private Sub rdbIT_CheckedChanged(sender As Object, e As EventArgs) Handles rdbIT.CheckedChanged
         position = "IT"
     End Sub
-
     Public Function checkDuplicate() As Boolean
         If isEmployeeDuplicate("emp_id", txtEmpID.Text) Then
             MessageBox.Show("รหัสพนักงานซ้ำ")
-            Return True
-        ElseIf isEmployeeDuplicate("firstname", txtFirstName.Text) Then
-            MessageBox.Show("ชื่อซ้ำ")
-            Return True
-        ElseIf isEmployeeDuplicate("lastname", txtLastName.Text) Then
-            MessageBox.Show("นามสกุลซ้ำ")
-            Return True
-        ElseIf isEmployeeDuplicate("phonenumber", txtPhoneNumber.Text) Then
-            MessageBox.Show("หมายเลขโทรศัพท์ซ้ำ")
             Return True
         ElseIf isEmployeeDuplicate("email", txtEmail.Text) Then
             MessageBox.Show("อีเมลซ้ำ")
@@ -115,7 +100,6 @@ Public Class EditUser
         End If
         Return False
     End Function
-
     Public Function isEmployeeDuplicate(ByVal field As String, ByVal text As String) As Boolean
         Dim isDup As Boolean = False
         Dim strSelect As String = "select " & field & " from SGS.dbo.Employee where " & field & " ='" & text.Trim & "' and id not in (" & id & ")"
@@ -123,11 +107,9 @@ Public Class EditUser
         con.close()
         Return isDup
     End Function
-
     Private Sub rdbAccountant_CheckedChanged(sender As Object, e As EventArgs) Handles rdbAccountant.CheckedChanged
         position = "Accountant"
     End Sub
-
     Private Sub saveData()
         Dim strquery = "update sgs.dbo.employee set "
         strquery &= " emp_id = '" & txtEmpID.Text.Trim & "' , "
@@ -150,17 +132,16 @@ Public Class EditUser
         Dim formSearchUser As New SearchUser
         formSearchUser.Show()
     End Sub
-
     Private Sub convertData()
-
-        IIf(position.Equals("IT"), rdbIT.Checked = True, rdbAccountant.Checked = True)
+        If position.Equals("IT") Then
+            rdbIT.Checked = True
+        Else : rdbAccountant.Checked = True
+        End If
         chbPerCreate.Checked = perCreate = 1
         chbPerDelete.Checked = perdelete = 1
         chbPerEdit.Checked = perEdit = 1
         cmbUserType.SelectedItem = IIf("admin".Equals(userType), "ผู้ดูแลระบบ", "ผู้ใช้งานทั่วไป")
-
     End Sub
-
     Private Sub loadData()
         Dim strQuery = "select id,"
         strQuery &= "emp_id,"
@@ -179,10 +160,11 @@ Public Class EditUser
         strQuery &= "where id = " & id
         sqlReader = con.query(strQuery)
     End Sub
-
     Private Sub initialData()
         txtEmail.Text = sqlReader.GetValue(sqlReader.GetOrdinal("email"))
+        oldData.Add("email", txtEmail.Text)
         txtEmpID.Text = sqlReader.GetValue(sqlReader.GetOrdinal("emp_id"))
+        oldData.Add("emp_id", txtEmpID.Text)
         txtFirstName.Text = sqlReader.GetValue(sqlReader.GetOrdinal("firstname"))
         txtLastName.Text = sqlReader.GetValue(sqlReader.GetOrdinal("lastname"))
         txtPassword.Text = sqlReader.GetValue(sqlReader.GetOrdinal("password"))
@@ -235,5 +217,59 @@ Public Class EditUser
                 e.Handled = True
                 MessageBox.Show("รหัสพนักงานต้องเป็นตัวอักษรภาษาอังกฤษและตัวเลขเท่านั้น")
         End Select
+    End Sub
+    Private Sub txtEmpID_correct()
+        lblMsgEmpId.Visible = True
+        pbEmpId.Visible = True
+        pbEmpId.Image = Nothing
+        pbEmpId.BackgroundImage = My.Resources.correct
+        lblMsgEmpId.ForeColor = Color.ForestGreen
+        lblMsgEmpId.Text = "สามารถใช้รหัสพนักงานนี้ได้"
+    End Sub
+    Private Sub txtEmpID_incorrect()
+        lblMsgEmpId.Visible = True
+        pbEmpId.Visible = True
+        pbEmpId.Image = Nothing
+        pbEmpId.BackgroundImage = My.Resources.incorrect
+        lblMsgEmpId.ForeColor = Color.Red
+        lblMsgEmpId.Text = "รหัสพนักงานซ้ำ"
+    End Sub
+    Private Sub txtEmail_incorrect(ByVal msg As String)
+        lblMsgEmail.Visible = True
+        pbEmail.Visible = True
+        pbEmail.Image = Nothing
+        pbEmail.BackgroundImage = My.Resources.incorrect
+        lblMsgEmail.ForeColor = Color.Red
+        lblMsgEmail.Text = msg
+    End Sub
+    Private Sub txtEmail_correct()
+        lblMsgEmail.Visible = True
+        pbEmail.Visible = True
+        pbEmail.Image = Nothing
+        pbEmail.BackgroundImage = My.Resources.correct
+        lblMsgEmail.ForeColor = Color.ForestGreen
+        lblMsgEmail.Text = "สามารถใช้อีเมลนี้ได้"
+    End Sub
+    Private Sub txtEmpID_LostFocus(sender As Object, e As EventArgs) Handles txtEmpID.LostFocus
+        If txtEmpID.Text Is String.Empty OrElse oldData("emp_id").Equals(txtEmpID.Text) Then
+            lblMsgEmpId.Visible = False
+            pbEmpId.Visible = False
+        ElseIf isEmployeeDuplicate("emp_id", txtEmpID.Text) AndAlso Not oldData("emp_id").Equals(txtEmpID.Text) Then
+            txtEmpID_incorrect()
+        ElseIf Not isEmployeeDuplicate("emp_id", txtEmpID.Text) AndAlso Not oldData("emp_id").Equals(txtEmpID.Text) AndAlso Not txtEmpID.Text Is String.Empty Then
+            txtEmpID_correct()
+        End If
+    End Sub
+    Private Sub txtEmail_LostFocus(sender As Object, e As EventArgs) Handles txtEmail.LostFocus
+        If txtEmail.Text Is String.Empty OrElse oldData("email").Equals(txtEmail.Text) Then
+            lblMsgEmail.Visible = False
+            pbEmail.Visible = False
+        ElseIf isEmployeeDuplicate("email", txtEmail.Text) AndAlso Not oldData("email").Equals(txtEmpID.Text) Then
+            txtEmail_incorrect("อีเมลซ้ำ")
+        ElseIf Not New Regex("^([0-9a-zA-Z]([-\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$").IsMatch(txtEmail.Text.Trim) Then
+            txtEmail_incorrect("รูปแบบอีเมลไม่ถูกต้อง")
+        ElseIf Not isEmployeeDuplicate("email", txtEmail.Text) AndAlso Not oldData("email").Equals(txtEmpID.Text) AndAlso Not txtEmail.Text Is String.Empty Then
+            txtEmail_correct()
+        End If
     End Sub
 End Class
