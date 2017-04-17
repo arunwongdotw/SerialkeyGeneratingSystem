e1d5fd8413cc067634749f938900248fb38a2dd4
pratya yeekhaday <zee_pratya@hotmail.com>
2017-04-17 14:31:26 +0700

    add check con when user close program

--- Serial Generating System/Administrator/CheckConnectUser.vb
+++ Serial Generating System/Administrator/CheckConnectUser.vb
@@ -2,7 +2,15 @@
 Public Class CheckConnectUser
 
     Private con As New ConnectDB
+    Private Sub sqlCloseConnection()
+        Dim sql = " update sgs.dbo.connection set active_flag = 0 where id = " & Login.idConnection
+        con.save(sql)
+    End Sub
 
+    Public Sub closeconnection() Handles Me.FormClosed
+        Dim close As New CheckConnectUser
+        close.sqlCloseConnection()
+    End Sub
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvConnection.CellContentClick
 
     End Sub
