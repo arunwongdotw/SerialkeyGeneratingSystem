1bc9c5e4e7dd7d67cecfbe544ae49a14bdd8afcc
pratya yeekhaday <zee_pratya@hotmail.com>
2017-05-10 17:03:12 +0700

    Edit data grid search serial

--- Serial Generating System/User/SearchSerial.vb
+++ Serial Generating System/User/SearchSerial.vb
@@ -125,9 +125,11 @@ Public Class SearchSerial
 
     Private Sub GenerateColumn()
         Try
+
             Me.dgvSerialKey.Columns.Clear()
             Me.dgvSerialKey.AutoGenerateColumns = False
             Me.dgvSerialKey.RowTemplate.MinimumHeight = 30
+
 
 
             Dim btnDelete As New DataGridViewButtonColumn()
@@ -143,6 +145,13 @@ Public Class SearchSerial
             Col.Width = 60
             Col.DataPropertyName = "seq"
             Col.Name = "seq"
+            Col.ReadOnly = True
+            Me.dgvSerialKey.Columns.Add(Col)
+
+            Col = New DataGridViewTextBoxColumn
+            Col.HeaderText = "ลำดับที่"
+            Col.Width = 100
+            Col.Name = "ลำดับที่"
             Col.ReadOnly = True
             Me.dgvSerialKey.Columns.Add(Col)
 
@@ -472,6 +481,13 @@ Public Class SearchSerial
             dgvSerialKey.Columns.Clear()
             GenerateColumn()
             dgvSerialKey.DataSource = dt
+            dgvSerialKey.Columns("seq").Visible = False
+            dgvSerialKey.Columns("ลำดับที่").HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
+            randerColorRow()
+            genRowNumber()
+
+
+
             'End If
             'Dim adapter As SqlDataAdapter = con.queryForAdapter(sql)
             'con.close()
@@ -483,6 +499,18 @@ Public Class SearchSerial
         Catch ex As Exception
             'MsgBox("ไม่พบข้อมูล")
         End Try
+    End Sub
+    Private Sub genRowNumber()
+        For i = 0 To dgvSerialKey.Rows.Count - 1
+            dgvSerialKey.Rows(i).Cells("ลำดับที่").Value = i + 1
+        Next
+    End Sub
+    Private Sub randerColorRow()
+        For i = 0 To dgvSerialKey.Rows.Count - 1
+            If i Mod 2 = 0 Then
+                dgvSerialKey.Rows(i).DefaultCellStyle.BackColor = Color.AliceBlue
+            End If
+        Next
     End Sub
 
     Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
