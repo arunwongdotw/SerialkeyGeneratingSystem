a0e2f0fd7a8a75c4c6d9c00678ebaaad1c85c823
PhirompornBussaban <phirompornbussaban@gmail.com>
2017-05-18 12:26:39 +0700

    Login UI (ยังไม่ครบค่ะ)

--- Serial Generating System/Login.Designer.vb
+++ Serial Generating System/Login.Designer.vb
@@ -33,17 +33,18 @@ Partial Class Login
         '
         'btnLogin
         '
-        Me.btnLogin.BackColor = System.Drawing.Color.WhiteSmoke
+        Me.btnLogin.BackColor = System.Drawing.Color.White
         Me.btnLogin.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(CType(CType(8, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(86, Byte), Integer))
         Me.btnLogin.FlatAppearance.BorderSize = 0
-        Me.btnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(128, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.btnLogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Azure
+        Me.btnLogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Azure
         Me.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnLogin.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLogin.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnLogin.ForeColor = System.Drawing.Color.FromArgb(CType(CType(8, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(86, Byte), Integer))
-        Me.btnLogin.Location = New System.Drawing.Point(36, 366)
+        Me.btnLogin.Location = New System.Drawing.Point(64, 370)
         Me.btnLogin.Margin = New System.Windows.Forms.Padding(3, 6, 3, 6)
         Me.btnLogin.Name = "btnLogin"
-        Me.btnLogin.Size = New System.Drawing.Size(164, 33)
+        Me.btnLogin.Size = New System.Drawing.Size(133, 34)
         Me.btnLogin.TabIndex = 3
         Me.btnLogin.Text = "เข้าสู่ระบบ"
         Me.btnLogin.UseVisualStyleBackColor = False
@@ -53,77 +54,81 @@ Partial Class Login
         Me.bntClose.BackColor = System.Drawing.Color.FromArgb(CType(CType(8, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(86, Byte), Integer))
         Me.bntClose.FlatAppearance.BorderColor = System.Drawing.Color.White
         Me.bntClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.bntClose.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bntClose.Font = New System.Drawing.Font("Segoe UI Semibold", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.bntClose.ForeColor = System.Drawing.Color.White
         Me.bntClose.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.bntClose.Location = New System.Drawing.Point(200, 366)
+        Me.bntClose.Location = New System.Drawing.Point(198, 370)
         Me.bntClose.Margin = New System.Windows.Forms.Padding(3, 6, 3, 6)
         Me.bntClose.Name = "bntClose"
-        Me.bntClose.Size = New System.Drawing.Size(164, 33)
+        Me.bntClose.Size = New System.Drawing.Size(133, 34)
         Me.bntClose.TabIndex = 4
         Me.bntClose.Text = "ปิดการใช้งาน"
         Me.bntClose.UseVisualStyleBackColor = False
         '
         'txtUsername
         '
-        Me.txtUsername.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(236, Byte), Integer))
+        Me.txtUsername.BackColor = System.Drawing.Color.White
         Me.txtUsername.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.txtUsername.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtUsername.ForeColor = System.Drawing.Color.FromArgb(CType(CType(8, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(86, Byte), Integer))
-        Me.txtUsername.Location = New System.Drawing.Point(124, 268)
+        Me.txtUsername.Location = New System.Drawing.Point(157, 256)
         Me.txtUsername.Margin = New System.Windows.Forms.Padding(3, 6, 3, 6)
         Me.txtUsername.Name = "txtUsername"
         Me.txtUsername.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.txtUsername.Size = New System.Drawing.Size(234, 23)
+        Me.txtUsername.Size = New System.Drawing.Size(173, 23)
         Me.txtUsername.TabIndex = 1
         '
         'txtPassword
         '
-        Me.txtPassword.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(236, Byte), Integer))
+        Me.txtPassword.BackColor = System.Drawing.Color.White
         Me.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.txtPassword.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.txtPassword.ForeColor = System.Drawing.Color.FromArgb(CType(CType(8, Byte), Integer), CType(CType(58, Byte), Integer), CType(CType(86, Byte), Integer))
-        Me.txtPassword.Location = New System.Drawing.Point(124, 324)
+        Me.txtPassword.Location = New System.Drawing.Point(157, 304)
         Me.txtPassword.Margin = New System.Windows.Forms.Padding(3, 6, 3, 6)
         Me.txtPassword.Name = "txtPassword"
         Me.txtPassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
         Me.txtPassword.RightToLeft = System.Windows.Forms.RightToLeft.No
         Me.txtPassword.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
-        Me.txtPassword.Size = New System.Drawing.Size(234, 23)
+        Me.txtPassword.Size = New System.Drawing.Size(173, 23)
         Me.txtPassword.TabIndex = 2
         '
         'btnLoginUsername
         '
-        Me.btnLoginUsername.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(236, Byte), Integer))
+        Me.btnLoginUsername.BackColor = System.Drawing.Color.White
         Me.btnLoginUsername.FlatAppearance.BorderSize = 0
+        Me.btnLoginUsername.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btnLoginUsername.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White
         Me.btnLoginUsername.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnLoginUsername.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLoginUsername.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnLoginUsername.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.btnLoginUsername.Location = New System.Drawing.Point(37, 265)
+        Me.btnLoginUsername.Location = New System.Drawing.Point(73, 253)
         Me.btnLoginUsername.Margin = New System.Windows.Forms.Padding(2, 3, 2, 3)
         Me.btnLoginUsername.Name = "btnLoginUsername"
-        Me.btnLoginUsername.Size = New System.Drawing.Size(83, 29)
+        Me.btnLoginUsername.Size = New System.Drawing.Size(79, 29)
         Me.btnLoginUsername.TabIndex = 5
         Me.btnLoginUsername.TabStop = False
         Me.btnLoginUsername.Text = "ชื่อผู้ใช้ :"
-        Me.btnLoginUsername.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btnLoginUsername.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
         Me.btnLoginUsername.UseVisualStyleBackColor = False
         '
         'btnLoginPassword
         '
-        Me.btnLoginPassword.BackColor = System.Drawing.Color.FromArgb(CType(CType(221, Byte), Integer), CType(CType(227, Byte), Integer), CType(CType(236, Byte), Integer))
+        Me.btnLoginPassword.BackColor = System.Drawing.Color.White
         Me.btnLoginPassword.FlatAppearance.BorderSize = 0
+        Me.btnLoginPassword.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btnLoginPassword.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White
         Me.btnLoginPassword.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.btnLoginPassword.Font = New System.Drawing.Font("Consolas", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnLoginPassword.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnLoginPassword.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
-        Me.btnLoginPassword.Location = New System.Drawing.Point(37, 318)
+        Me.btnLoginPassword.Location = New System.Drawing.Point(66, 299)
         Me.btnLoginPassword.Margin = New System.Windows.Forms.Padding(1)
         Me.btnLoginPassword.Name = "btnLoginPassword"
-        Me.btnLoginPassword.Size = New System.Drawing.Size(83, 25)
+        Me.btnLoginPassword.Size = New System.Drawing.Size(86, 25)
         Me.btnLoginPassword.TabIndex = 6
         Me.btnLoginPassword.TabStop = False
         Me.btnLoginPassword.Text = "รหัสผ่าน :"
-        Me.btnLoginPassword.TextAlign = System.Drawing.ContentAlignment.TopRight
+        Me.btnLoginPassword.TextAlign = System.Drawing.ContentAlignment.TopLeft
         Me.btnLoginPassword.UseVisualStyleBackColor = False
         '
         'Login
@@ -131,9 +136,9 @@ Partial Class Login
         Me.AutoScaleDimensions = New System.Drawing.SizeF(10.0!, 22.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.DarkSlateGray
-        Me.BackgroundImage = Global.Serial_Generating_System.My.Resources.Resources.bgLoginVersion2
+        Me.BackgroundImage = Global.Serial_Generating_System.My.Resources.Resources.bgLoginND
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(398, 483)
+        Me.ClientSize = New System.Drawing.Size(398, 500)
         Me.Controls.Add(Me.btnLoginPassword)
         Me.Controls.Add(Me.btnLoginUsername)
         Me.Controls.Add(Me.txtPassword)
